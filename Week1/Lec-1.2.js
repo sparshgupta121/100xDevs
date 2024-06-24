261ccc48b2da22ec45be6ec78669f2f4388ec18c
sparshgupta121 <sparshdheeeraj@gmail.com>
2024-06-24 23:00:11 +0530

    Week 1 completed

--- Week1/Lec-1.2.js
+++ Week1/Lec-1.2.js
@@ -5,3 +5,23 @@ var a=2
 console.log(a);
 
 //issue with var can be redeclared can be changed system may get confused
+
+let Firstname = "Sparsh"
+let isMarried = false
+
+if(isMarried){
+    console.log(`${Firstname} is married`);
+}
+
+else{
+    console.log(`${Firstname} is not married`);
+}
+
+let ans= 0;
+
+for (let index = 1; index <=6; index++) {
+    ans=ans+index
+    
+}
+
+console.log(ans);
