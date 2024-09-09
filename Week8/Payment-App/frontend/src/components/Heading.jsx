caf1cae06f2aa36a15351b65b1ea624a32beb07e
Sparsh Gupta <125153117+sparshgupta121@users.noreply.github.com>
2024-09-09 11:16:01 +0530

    Delete Week8/Payment-App directory

--- Week8/Payment-App/frontend/src/components/Heading.jsx
+++
@@ -1,3 +0,0 @@
-export function Heading({ label }) {
-	return <div className='font-bold text-3xl pt-4 text-center'>{label}</div>;
-}
--- Week8/Payment-App/frontend/src/components/SubHeading.jsx
+++
@@ -1,3 +0,0 @@
-export function SubHeading({ label }) {
-	return <div className='text-slate-600 text-md pt-1 mt-2 text-center'>{label}</div>;
-}
